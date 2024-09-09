082f2c55bc543267d2d6d652b104b6395c9113d0
Muhammad Raheel <103869166+muhammad-raheel-fsd@users.noreply.github.com>
2024-09-09 18:28:20 +0500

    day 28: let's start js

---
+++ 03-js/01-values-variables-and-operators/js/app.js
@@ -0,0 +1,48 @@
+// Values
+
+// String
+console.log("Js in fun");
+
+// Number
+console.log(100);
+
+// Booleans
+console.log(true);
+console.log(false);
+
+// Null
+console.log(null);
+
+// Undefined
+console.log(undefined);
+
+// Datatypes in js - 7
+// 1- String
+// 2- Number
+// 3- Boolean
+// 4- Null
+// 5- Undefined
+// 6- Symbol
+// 7- BigInt
+
+// paple
+// apple
+
+// Variables
+age = 88;
+console.log(typeof age);
+
+age = "100";
+console.log(typeof age);
+
+// console.log(age)
+// console.log(age)
+// console.log(age)
+// console.log(age)
+// console.log(age)
+
+// keywords
+
+// 1- var
+// 2- let
+// 3- const
