a1fec01ee3033d6c6b095e1ba8e70b6f90e452da
Muhammad Raheel <103869166+muhammad-raheel-fsd@users.noreply.github.com>
2024-09-20 17:44:05 +0500

    day 34: arrays and loops

--- 03-js/js/02-more-on-fundamentals/app.js
+++ 03-js/js/02-more-on-fundamentals/app.js
@@ -110,14 +110,37 @@ console.log(loggerHere);
 
 const arr = ["Rehman", NaN, true, 10, "Shahid", undefined];
 
-console.log("Array =======", arr);
-console.log("Array =======", arr.length);
-console.log("Array value =======", arr[1]);
-console.log("Array value =======", arr[5]);
-console.log("Array value =======", arr[arr.length / 2 - 1]);
-console.log("Array value =======", arr[arr.length - 1]);
-console.log("Array value =======", arr[arr.length - arr.length]);
-console.log("Array value =======", arr[0]);
+// console.log("Array =======", arr);
+// console.log("Array =======", arr.length);
+// console.log("Array value =======", arr[1]);
+// console.log("Array value =======", arr[5]);
+// console.log("Array value =======", arr[arr.length / 2 - 1]);
+// console.log("Array value =======", arr[arr.length - 1]);
+// console.log("Array value =======", arr[arr.length - arr.length]);
+// console.log("Array value =======", arr[0]);
+
+// for(let index = 0; index < arr.length; index++){
+//   console.log("Array value =======", arr[index])
+// }
+
+const multiArray = [1, "name", [1, 2, 4, null]];
+// Recursion
+console.log(multiArray);
+
+const tabularArray = [
+  [1, 2, 3, 4, 5, 6, 7, 8, 9, 10],
+  [11, 12, 13, 14, 15, 16, 17, 18, 19, 20],
+  [21, 22, 23, 24, 25, 26, 27, 28, 29, 30],
+  [31, 32, 33, 34, 35, 36, 37, 38, 39, 40],
+  [41, 42, 43, 44, 45, 46, 47, 48, 49, 50],
+];
+
+for (let outerIndex = 0; outerIndex < tabularArray.length; outerIndex++) {
+  const innerArray = tabularArray[outerIndex];
+  for (let innerIndex = 0; innerIndex < innerArray.length; innerIndex++) {
+    console.log(`${outerIndex} x ${innerIndex} ===> ${innerArray[innerIndex]}`);
+  }
+}
 
 // Mid value
 // const mid = arr[arr.length / 2];
@@ -151,16 +174,63 @@ console.log(midAndAvgOfNumbers(numbers));
 
 // }
 
-const count = 5;
+const numArray = [10, 20, 30, 40, 50, 60, 70, 80];
+for (let counter = 1; counter <= numArray.length; counter++) {
+  console.log("I'm learning JS");
+}
+
+// let whileCounter = 1;
+// while (whileCounter <= 5) {
+let initiator = 0;
+// while (initiator <= numArray.length) {
+while (false) {
+  console.log("While...");
+  // whileCounter++;
+  initiator++;
+}
+
+// 3- do while loop
+let doWhileInitiator = 1;
+do {
+  console.log("Do while...");
+  doWhileInitiator++;
+} while (false);
+
+let count = 1;
 // post increment / decrement
-// count++
+count++;
+count = count + 1;
 // pre increment
 // ++count;
 // console.log("count", ++count);
 // console.log("count", count++);
 
-console.log("count", --count);
-// console.log("count", count);
+count = count - 1;
+count += count;
+
+// Example
+// Table of 2
+
+// 2 * 1 = 2
+// 2 * 2 = 4
+// 2 * 3 = 6
+// 2 * 4 = 8
+
+// continue, break
+
+for (let counter = 1; counter < 11; counter++) {
+  // continue;
+  // if (counter === 5) continue;
+  if (counter === 5) break;
+  console.log(`2 * ${counter} = ${counter * 2}`);
+}
+
+// for (let counter = 10; counter >= 1; counter--) {
+//   console.log(`2 * ${counter} = ${counter * 2}`);
+// }
+
+// console.log("count", --count);
+console.log("count", count);
 // Ternary operator (expression)
 
 const condition = true;
