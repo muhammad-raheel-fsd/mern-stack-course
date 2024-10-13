30cbfeee60c6588161ed3ad27b4a79593d4c8736
Muhammad Raheel <103869166+muhammad-raheel-fsd@users.noreply.github.com>
2024-10-13 14:16:38 +0500

    day 40: dom manipulations and styling

---
+++ 03-js/js/00-practice/app.js
@@ -0,0 +1,6 @@
+const main = document.getElementById("main");
+const button = document.getElementById("button");
+
+button.addEventListener("click", function (event) {
+  main.style.backgroundColor = "purple";
+});
--- 03-js/js/04-dom-manipulation/app.js
+++ 03-js/js/04-dom-manipulation/app.js
@@ -47,6 +47,49 @@ const moreQueryElements = document.querySelector("#title"); // id
 // document.querySelectorAll("")
 console.log(moreQueryElements);
 
-// Getting text
+// Practice
+const classElements = document.getElementsByClassName("content");
+const moreClassElements = document.querySelectorAll(".content");
+const moreElementsWithQuery = document.querySelectorAll("#text");
+
+console.log({ classElements, moreClassElements, moreElementsWithQuery });
+
+const singleElementWithQuery = document.querySelector(".content");
+const singleIdElementWithQuery = document.querySelector("#text");
+console.log({ singleElementWithQuery, singleIdElementWithQuery });
+singleElementWithQuery.innerText = "Some text here...";
+
+// Multiple elements
+// Getting and setting text
+moreElementsWithQuery.innerText = "Some text here...";
+moreElementsWithQuery[0].innerText = "Some text here...";
+moreElementsWithQuery[1].innerText = "Some text here...";
+moreElementsWithQuery[2].innerText = "Some text here...";
+
+moreClassElements.forEach(function (element) {
+  element.innerText = "Passing text to multiple elements...";
+});
 
 // Style
+singleElementWithQuery.style.color = "red";
+singleElementWithQuery.style.backgroundColor = "green";
+singleElementWithQuery.style.padding = "20px";
+singleElementWithQuery.style.border = "20px";
+singleElementWithQuery.style.borderStyle = "solid";
+// singleElementWithQuery.style.borderLeftColor = "orange";
+
+console.log({ style: singleElementWithQuery.style });
+
+// Event listeners
+
+const buttonClickListener = function (event) {
+  console.log(event);
+  console.log("buttonClickListener");
+};
+
+// Button here
+
+const button = document.getElementById("button");
+button.addEventListener("click", function (event) {
+  console.log(event, "Clicked...");
+});
