37356655885c77c14f559893a6d81ab3df3c4038
Muhammad Raheel <103869166+muhammad-raheel-fsd@users.noreply.github.com>
2024-11-13 19:01:53 +0500

    day 52: fetch & code execution order

--- 03-js/js/08-js-promises/app.js
+++ 03-js/js/08-js-promises/app.js
@@ -1,30 +1,8 @@
 const excuse = false;
 
-const letsHaveSomeTea = new Promise((resolve, reject) => {
-  if (excuse) {
-    reject("I'm sorry, I can't make it today.");
-  } else {
-    resolve("Hey, let's have some tea today.");
-  }
-});
-
-letsHaveSomeTea.then((message) => console.log(message));
-letsHaveSomeTea.catch((error) => console.log(error));
-
-console.log(letsHaveSomeTea);
-
-// const excuse = true;
-// new Promise((resolve, reject) => {
-//   if (excuse) reject("Just rejected");
-//   resolve("Let's do it");
-// })
-//   .then((message) => {
-//     console.log(message);
-//   })
-//   .catch((err) => console.log(err));
-
 const resolveButton = document.getElementById("resolve-button");
 const rejectButton = document.getElementById("reject-button");
+const dataButton = document.getElementById("data-button");
 
 const promise = new Promise((resolve, reject) => {
   resolveButton.addEventListener("click", () => {
@@ -36,9 +14,77 @@ const promise = new Promise((resolve, reject) => {
 
   setTimeout(() => {
     reject("Promise rejected...");
-  }, 5000);
+  }, 0);
 });
 
 promise
   .then((message) => console.log(message))
   .catch((err) => console.log(err));
+
+const letsHaveSomeTea = new Promise((resolve, reject) => {
+  if (excuse) {
+    reject("I'm sorry, I can't make it today.");
+  } else {
+    resolve("Hey, let's have some tea today.");
+  }
+});
+
+letsHaveSomeTea.then((message) => console.log(message));
+letsHaveSomeTea.catch((error) => console.log(error));
+
+// console.log(letsHaveSomeTea);
+
+// const excuse = true;
+// new Promise((resolve, reject) => {
+//   if (excuse) reject("Just rejected");
+//   resolve("Let's do it");
+// })
+//   .then((message) => {
+//     console.log(message);
+//   })
+//   .catch((err) => console.log(err));
+
+const showSomething = () => console.log("Arrow log...");
+
+console.log("Log 1...");
+
+showSomething();
+
+console.log("Log 2...");
+
+// Line by line 1, 2, 3...
+
+// 1- Default
+// 2 - Promises
+// 3- Timers
+
+// API's (Application Programming Interface)
+
+// https://jsonplaceholder.typicode.com/users
+
+// XHR (XMLHttpRequest)
+// Fetch (window.fetch)
+
+// window
+//   .fetch("https://jsonplaceholder.typicode.com/users") // GET
+//   .then((result) => {
+//     return result.json(); // Convert to JSON
+//   })
+//   .then((data) => console.log(data)) //Data
+//   .catch((err) => console.log(err));
+
+// let apiData;
+
+dataButton.addEventListener("click", (event) => {
+  window
+    .fetch("https://jsonplaceholder.typicode.com/users")
+    .then((response) => response.json())
+    .then((data) => {
+      console.log(data);
+    })
+    .catch((err) => console.log(err));
+});
+
+// console.log(apiData);
+
+// then, catch
