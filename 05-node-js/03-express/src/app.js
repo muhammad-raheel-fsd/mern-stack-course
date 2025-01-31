8f8fec04fa852b28864a5a206f53f22be68e8d6a
Muhammad Raheel <103869166+muhammad-raheel-fsd@users.noreply.github.com>
2025-01-31 15:52:01 +0500

    day 73: searching and filtering

--- 05-node-js/03-express/src/app.js
+++ 05-node-js/03-express/src/app.js
@@ -1,16 +1,60 @@
 import http from "http";
 import express from "express";
 import dotenv from "dotenv";
+import { db } from "./data/db.js";
+
+const arr = ["raheel", "rehman", "basit", "usman"];
+
+// const str = "Allexander";
+
+// console.log("includes ========>", str.search("z"));
+
+console.log(
+  "every",
+  arr.every((name) => name === "usman")
+);
+console.log(
+  "some",
+  arr.some((name) => name === "usman")
+);
+
+// const arr = [
+//   { id: 1, name: "raheel" },
+//   { id: 2, name: "rehman" },
+//   { id: 3, name: "basit" },
+//   { id: 4, name: "usman" },
+// ];
+
+// console.log(arr.find((user) => user.name === "basit"));
 
 dotenv.config();
 const PORT = process.env.PORT ?? 8080;
 
 const app = express(http.createServer());
 
-app.get("/", (req, res) => {
-  //   res.header("Content-Type", "application/json");
-  res.status(200).send({ message: "Hello World" });
-  //   res.send({ message: "Hello World" });
+app.get("/homes", (req, res) => {
+  const query = req.query;
+  if (query.search) {
+    const homes = db.homes.filter((home) => {
+      return home.title.toLowerCase().search(query.search.toLowerCase()) !== -1;
+    });
+    return res.status(200).json(homes);
+  }
+  if (Object.keys(query).length > 0) {
+    const homes = db.homes.filter((home) => {
+      return Object.keys(query).every((key) => {
+        return home[key] === query[key];
+      });
+    });
+    return res.status(200).json(homes);
+  }
+  return res.status(200).json(db.homes);
+});
+
+app.get("/homes/:id", (req, res) => {
+  const { id } = req.params;
+  const home = db.homes.find((home) => home.id === id);
+  res.status(200).json(home);
 });
 
 app.post("/", (req, res) => {
